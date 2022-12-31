0d5dc95c8d8da4d1f3bb6100b717a3d4a9d9432d
moraesvictor <moraesvictorgk@gmail.com>
2022-12-31 09:09:43 -0300

    chore: add context definition

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import fetch from 'node-fetch';
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  // através do context é possível compartilhar
+  // a mesma função/objeto/dado dentre seus inúmeros resolvers.
   context: () => {
     return {
       fetch,
