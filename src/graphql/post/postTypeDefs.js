7f16ae8cb1e7343c397c745bc4b3612bc8bb5b4a
moraesvictor <moraesvictorgk@gmail.com>
2023-01-13 23:56:15 -0300

    chore: link user to post

--- src/graphql/post/postTypeDefs.js
+++ src/graphql/post/postTypeDefs.js
@@ -10,10 +10,9 @@ export const postTypeDefs = gql`
     id: ID!
     title: String!
     body: String!
-    # user: User!
+    user: User!
     indexRef: Int!
     createdAt: String!
-    unixTimestamp: String!
   }
 
   ## Interface ##
